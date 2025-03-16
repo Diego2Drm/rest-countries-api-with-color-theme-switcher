2ca8d3523ce9498b078230caeaaf94aa624f57b8
Diego2dRamirez <diego.ramirez2m03@gmail.com>
2025-03-15 21:33:45 -0600

    Project Initialization and Tailwind Configuration

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { FaAddressCard } from "react-icons/fa";
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-red-400 text-3xl font-bold text-center">Rest Countries API</h1>
+      <FaAddressCard />
+
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        // Dark Mode
+        "Dark-Blue": "#2b3945", /*(Dark Mode Elements)*/
+        "Very-Dark-Blue": "#202c37", /*(Dark Mode Background)*/
+        //Light Mode
+        "Very-Dark-Blue": "#111517", /*(Light Mode Text)*/
+        "Dark-Gray": "#858585", /*(Light Mode Input)*/
+        "Very-Light Gray": "#fafafa", /*(Light Mode Background)*/
+        // "White": "#ffffff", /*(Light Mode Top Background)*/
+      },
+      fontFamily: {
+        "Nunito": ["Nunito Sans", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
+
